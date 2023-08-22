d3f155d80d277c69d9fafbbe168c29c0da16e6cb
Riesen95 <gumakow@gmail.com>
2023-08-22 18:00:50 +0200

    Pics

---
+++ .idea/.idea.ZooAPI/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+E
+frontend/frontend.txt,a\d\addb9e2519e41ebaa85ece9aa4a8a9a7f9188c6a
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+﻿import { Box, Typography, Paper } from '@mui/material';
+import Layout from '../../layout/Layout';
+import ServiceList from '../../src/services/ServiceList';
+
+const HomePage = () => {
+    return (
+        <Layout>
+            <Box sx={{mt: 4, mb: 4, textAlign: 'center'}}>
+                <Typography variant="h4" gutterBottom>
+                    Willkommen im Zoo
+                </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '720px' }}>
+                <Paper elevation={3} sx={{ p: 4, borderRadius: 2, backgroundColor: 'rgba(155,155,155,0.9)', width: '100%', maxWidth: '1920px' }}>
+                    <Typography variant="h5" gutterBottom>
+                        Wählen Sie Ihren Service
+                    </Typography>
+                    <ServiceList />
+                </Paper>
+            </Box>
+        </Layout>
+    );
+};
+
+export default HomePage;
